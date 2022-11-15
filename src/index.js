38251e379a56408ab90843d43fe5318fab64de85
ju <julienbmartel@gmail.com>
2022-11-15 16:11:52 -0600

    fix: module

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { usePuppeteer } from "./use-puppeteer.js";
-import { getArchillectImage } from "./utils";
+import { getArchillectImage } from "./utils.js";
 
 const { browser, page } = await usePuppeteer();
 
