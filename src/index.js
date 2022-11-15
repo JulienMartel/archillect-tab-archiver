c67b3aa634f4e2d07014feb6a452f0cbfac01cc0
ju <julienbmartel@gmail.com>
2022-11-15 15:53:53 -0600

    fix: url

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { usePuppeteer } from "./use-puppeteer.js";
 
 const { browser, page } = await usePuppeteer();
 
-await page.goto("https://www.google.com", { waitUntil: "domcontentloaded" });
+await page.goto("https://archillect.com/1", { waitUntil: "domcontentloaded" });
 
 await page.waitForSelector("img#ii");
 const src = await page.$eval("img#ii", (el) => el.src);
