0031d26ee2f0cf28ccbc04315501630ae7389dba
ju <julienbmartel@gmail.com>
2022-11-15 21:07:29 -0600

    testing fetching 'from' from mongo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,12 @@ const scrape = async (from, max) => {
 
   const { browser, page } = await usePuppeteer();
 
-  const x = db.collection("data").find().sort({ _id: -1 }).limit(1).toArray();
+  const x = await db
+    .collection("data")
+    .find()
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray();
   console.log(x);
 
   // const end = max + from;
