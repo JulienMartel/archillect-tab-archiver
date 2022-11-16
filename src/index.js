80d056899d8713bd2cf28acf4833750aae4cce90
ju <julienbmartel@gmail.com>
2022-11-15 21:10:22 -0600

    test new mongo read

--- src/index.js
+++ src/index.js
@@ -3,37 +3,37 @@ import { db } from "./mongo.js";
 import { usePuppeteer } from "./use-puppeteer.js";
 import { getArchillectImage } from "./utils.js";
 
-const scrape = async (from, max) => {
+const scrape = async (max) => {
   const spinner = ora("loading browser").start(); // `scraping #${id}/${end}`
 
   const { browser, page } = await usePuppeteer();
 
-  const x = await db
+  const [lastEntry] = await db
     .collection("data")
     .find()
     .sort({ _id: -1 })
     .limit(1)
     .toArray();
-  console.log(x);
 
-  // const end = max + from;
+  const from = lastEntry._id;
+  const end = max + from;
 
-  // let c = from;
-  // while (c <= end) {
-  //   try {
-  //     spinner.text = `scraping #${c}/${end}`;
-  //     await getArchillectImage(page, c);
-  //   } catch (error) {
-  //     spinner.fail("failed to get #" + c);
-  //     console.error(error);
-  //     spinner.start();
-  //   } finally {
-  //     c++;
-  //   }
-  // }
+  let c = from;
+  while (c <= end) {
+    try {
+      spinner.text = `scraping #${c}/${end}`;
+      await getArchillectImage(page, c);
+    } catch (error) {
+      spinner.fail("failed to get #" + c);
+      console.error(error);
+      spinner.start();
+    } finally {
+      c++;
+    }
+  }
 
-  // return await browser.close();
+  return await browser.close();
 };
 
 // make it so that i read where to start it from
-await scrape(301, 99);
+await scrape(10);
